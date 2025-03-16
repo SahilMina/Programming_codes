e584d440ce93db3e96dc9143479bd2a9bf415b43
Sahil Mina <sahilmina55555@gmail.com>
2025-03-16 19:49:16 +0530

    Assignment added

---
+++ Java_programs/Assignment/Q26_27.java
@@ -0,0 +1,20 @@
+public class Q26_27 {
+
+  public static void main(String[] args) {
+    int Integer = 100;
+    float floatNum = 10.5f;
+    double doubleNum = 20.99;
+    char letter = 'A';
+    String message = "Hello, Java!";
+    boolean True = true;
+    boolean False = false;
+    System.out.println("Types of litereal values in Java:");
+    System.out.println("Integer: " + Integer);
+    System.out.println("Float: " + floatNum);
+    System.out.println("Double: " + doubleNum);
+    System.out.println("Character: " + letter);
+    System.out.println("String: " + message);
+    System.out.println("True: " + True);
+    System.out.println("False: " + False);
+  }
+}
